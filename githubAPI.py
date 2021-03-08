86eee93523f258aefa3aa773bcc5b6d6b8c58ba6
Mark Paris <58420689+markparis1@users.noreply.github.com>
2021-03-08 15:38:12 -0500

    repositories print

--- githubAPI.py
+++ githubAPI.py
@@ -1,2 +1,17 @@
-import 
+import requests
 import json
+
+def repositories(id: str):
+    repos: [str] = []
+    r = requests.get(f"https://api.github.com/users/{id}/repos")
+    if not (r.status_code == 200):
+        print("Error could not find github id")
+    else:
+        github_json_repos = r.json()
+        for item in github_json_repos:
+            for key in item:
+            
+                if key == "name":
+                    repos.append(item[key])
+        return repos
+print(repositories("markparis1"))
