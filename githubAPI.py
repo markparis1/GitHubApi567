7c26928aa8be245caaf135d26e377ef952d058df
Mark Paris <58420689+markparis1@users.noreply.github.com>
2021-03-08 15:57:05 -0500

    Update githubAPI.py

--- githubAPI.py
+++ githubAPI.py
@@ -1,17 +1,49 @@
 import requests
 import json
+from typing import Dict
 
 def repositories(id: str):
+
     repos: [str] = []
     r = requests.get(f"https://api.github.com/users/{id}/repos")
+
     if not (r.status_code == 200):
+
         print("Error could not find github id")
+
     else:
+
         github_json_repos = r.json()
+
         for item in github_json_repos:
+
             for key in item:
-            
+
                 if key == "name":
+
                     repos.append(item[key])
-        return repos
+
+
+    
+    repos_commits = {}
+    for repo in repos:
+       
+        r = requests.get(f"https://api.github.com/repos/{id}/{repo}/commits")
+
+        if not (r.status_code == 200):
+
+            print("Error could not find github repository")
+
+        else:s
+
+            github_json_repos = r.json()
+            repos_commits[repo] = len(github_json_repos)
+        
+    return repos_commits
+
+            
+
+
+
+
 print(repositories("markparis1"))
